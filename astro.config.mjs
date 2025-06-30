1ca41b53a89ac84b62fc443b0db7c23c857db841
Andres carvajal <127360563+AndrewCarvajal97@users.noreply.github.com>
2025-06-30 11:50:56 -0500

    fix: resolve all static assets and deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     port: 3000
   },
-  site: 'https://andrewcarvajal97.github.io/frontend_Hackaton/',
+  site: 'https://andrewcarvajal97.github.io',
+  base: '/frontend_Hackaton/',
   output: 'static'
 });
